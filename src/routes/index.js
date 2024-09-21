86007d69c612da92e6f8743444c66ed0d914372d
phucdeptrai <tranhoangphuchatien07@gmail.com>
2024-09-21 09:34:20 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import NotFoundPage from "../components/NotFoundPage/NotFoundPage";
+import HomePage from "../pages/HomePage/HomePage";
+import OrderPage from "../pages/OrderPage/OrderPage";
+import ProductsPage from "../pages/ProductsPage/ProductsPage";
+
+export const routes = [
+    {
+        path: '/',
+        page: HomePage,
+        isShowHeader: true
+    },
+    {
+        path: '/order',
+        page: OrderPage,
+        isShowHeader: true
+    },
+    {
+        path: '/products',
+        page: ProductsPage,
+        isShowHeader: true
+    },
+    {
+        path: '*',
+        page: NotFoundPage
+    },
+]
