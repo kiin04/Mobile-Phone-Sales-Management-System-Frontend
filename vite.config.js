1f24985c3c87876fbd47bccd7543f1251a0607c3
Rin <lamtherin25072004@gmail.com>
2024-09-23 18:42:40 +0700

    update port v2

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
+// export default defineConfig({
+//   server: {
+//     port: 3001,  // Thay thế 3001 bằng cổng bạn muốn sử dụng
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5081,
     proxy: {
       '/api': {
-        target: 'http://localhost:3002', // Địa chỉ backend
+        target: 'http://localhost:5082', // Địa chỉ backend
         changeOrigin: true,
         secure: true
       },
