f6af728c70f81740139dbec1d695cab649f976f8
phucdeptrai <tranhoangphuchatien07@gmail.com>
2024-09-25 14:58:30 +0700

    update port v.2

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5081,
+    port: 5085,
     proxy: {
       '/api': {
         target: 'http://localhost:5082', // Địa chỉ backend
